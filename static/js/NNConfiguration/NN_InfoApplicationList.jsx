1f664666e045be6e4974ac30c2b35869e7669243
healess <healess1@gmail.com>
2017-09-25 20:48:49 +0900

    Remove unnessary files

--- static/js/NNConfiguration/NN_InfoApplicationList.jsx
+++ static/js/NNConfiguration/NN_InfoApplicationList.jsx
@@ -102,21 +102,33 @@ export default class NN_InfoApplicationList extends React.Component {
         /////////////////////////////////////////////////////////////////////////////////////////
         if (this.state.NN_TableMaster == null){
             this.state.NN_TableMaster = [   
-                                            {title:"Chatbot ID" , width:10 , input_data:"cb0002", ex:"ex) chatbot id"}
-                                            ,{title:"Chatbot Category" , width:10 , input_data:"service", ex:"ex) Category"}
-                                            ,{title:"Chatbot SubCategory" , width:10 , input_data:"info_bot", ex:"ex) Sub"}
-                                            ,{title:"Tagging Type" , width:10  , input_data:"dict", ex:"ex) Tagging Info"}
-                                            ,{title:"Proper Noun" , width:10 , input_data:"{'tagdate': [1, '/hoya_model_root/chatbot/date.txt', False]}", ex:"ex) Proper Noun"}
-                                            ,{title:"Intent Model" , width:10 , input_data:"wcnntest02", ex:"ex) Intent Model Name"}
-                                            ,{title:"NER Model" , width:10 , input_data:"lstmcrf0002", ex:"ex) NER Model Name"}
-                                            ,{title:"Intent ID" , width:10 , input_data:"1", ex:"ex) Intent"}
-                                            ,{title:"entity_type" , width:10 , input_data:"key", ex:""}
+                                            {title:"Chatbot ID" , width:10 , input_data:"cb0002", ex:"chatbot id"}
+                                            ,{title:"Chatbot Category" , width:10 , input_data:"service", ex:"Category"}
+                                            ,{title:"Chatbot SubCategory" , width:10 , input_data:"info_bot", ex:"Sub"}
+                                            ,{title:"Tagging Type" , width:10  , input_data:"dict", ex:"Tagging Info"}
+                                            ,{title:"Proper Noun" , width:10 , input_data:"{'tagdate': [1, '/hoya_model_root/chatbot/date.txt', False]}", ex:"Proper Noun"}
+                                         ];
+        }
+
+        if (this.state.NN_TableMaster2 == null){
+            this.state.NN_TableMaster2 = [   
+
+                                            {title:"Intent Model" , width:10 , input_data:"wcnntest02", ex:"Intent Model Name"}
+                                            ,{title:"Intent ID" , width:10 , input_data:"1", ex:"Intent"}
+                                            ,{title:"entity_type" , width:10 , input_data:"key", ex:"Key, extra"}
                                             ,{title:"entity_list" , width:10 , input_data:"{'key': ['tagdate', 'tagloc', 'tagmenu']}", ex:"JSON Format"}
-                                            ,{title:"Story ID" , width:10 , input_data:"1", ex:"ex) 1,2,3,4,5"}
-                                            ,{title:"Story Type" , width:10 , input_data:"response", ex:"ex) response / default"}
-                                            ,{title:"Entity Type" , width:10 ,input_data:"key", ex:"ex) "}
-                                            ,{title:"Response Type" , width:10 , input_data:"entity", ex:"ex) entity / default"}
-                                            ,{title:"Output Entity" , width:10 , input_data:"{'entity':['tagdate','tagloc','tagmenu']}", ex:"ex) "}
+                                         ];
+        }
+
+
+        if (this.state.NN_TableMaster3 == null){
+            this.state.NN_TableMaster3 = [   
+                                            {title:"Story ID" , width:10 , input_data:"1", ex:"1,2,3,4,5"}
+                                            ,{title:"Story Type" , width:10 , input_data:"response", ex:"response / default"}
+                                            ,{title:"Entity Type" , width:10 ,input_data:"key", ex:"ex) key, extra"}
+                                            ,{title:"Output_Entity" , width:10 ,input_data:"{'entity':['tagdate','tagloc','tagmenu']}", ex:"display entity"}
+                                            ,{title:"output_data" , width:10 ,input_data:"주문이 완료", ex:"display sentence"}
+                                            ,{title:"Response Type" , width:10 , input_data:"entity", ex:"entity / default"}
                                          ];
         }
 
@@ -140,36 +152,32 @@ export default class NN_InfoApplicationList extends React.Component {
                                                         defaultValue = {row["input_data"]}
                                                         maxLength = {row["width"]}  />  </td>)
             colData.push(<td key={k++} style={{"textAlign":"left", "width":"30%"}} > {row["ex"]} </td>)
-
             tableData.push(<tr key={k++}>{colData}</tr>)
         }
 
         let tableData2 = []
-        for(let rows in this.state.NN_TableMaster){
+        for(let rows in this.state.NN_TableMaster2){
             let colData = [];
-            let row = this.state.NN_TableMaster[rows]
+            let row = this.state.NN_TableMaster2[rows]
             colData.push(<td key={k++} style={{ "width":"20%"}}> {row["title"]} </td>)
             colData.push(<td key={k++} > < input type = {"string"} style={{"textAlign":"center", "width":"100%"}} 
                                                         defaultValue = {row["input_data"]}
                                                         maxLength = {row["width"]}  />  </td>)
             colData.push(<td key={k++} style={{"textAlign":"left", "width":"30%"}} > {row["ex"]} </td>)
-
             tableData2.push(<tr key={k++}>{colData}</tr>)
         }
 
         let tableData3 = []
-        for(let rows in this.state.NN_TableMaster){
+        for(let rows in this.state.NN_TableMaster3){
             let colData = [];
-            let row = this.state.NN_TableMaster[rows]
+            let row = this.state.NN_TableMaster3[rows]
             colData.push(<td key={k++} style={{ "width":"20%"}}> {row["title"]} </td>)
             colData.push(<td key={k++} > < input type = {"string"} style={{"textAlign":"center", "width":"100%"}} 
                                                         defaultValue = {row["input_data"]}
                                                         maxLength = {row["width"]}  />  </td>)
             colData.push(<td key={k++} style={{"textAlign":"left", "width":"30%"}} > {row["ex"]} </td>)
-
             tableData3.push(<tr key={k++}>{colData}</tr>)
         }
-
 
         let masterListTable = []
         masterListTable.push(<thead ref='thead' key={k++} className="center">{tableHeader}</thead>)
@@ -177,12 +185,11 @@ export default class NN_InfoApplicationList extends React.Component {
 
         let intentInfoTable = []
         intentInfoTable.push(<thead ref='thead' key={k++} className="center">{tableHeader}</thead>)
-        intentInfoTable.push(<tbody ref='tbody' key={k++} className="center" >{tableData}</tbody>)
+        intentInfoTable.push(<tbody ref='tbody' key={k++} className="center" >{tableData2}</tbody>)
 
-        let nerInfoTable = []
-        nerInfoTable.push(<thead ref='thead' key={k++} className="center">{tableHeader}</thead>)
-        nerInfoTable.push(<tbody ref='tbody' key={k++} className="center" >{tableData}</tbody>)
-
+        let storyInfoTable = []
+        storyInfoTable.push(<thead ref='thead' key={k++} className="center">{tableHeader}</thead>)
+        storyInfoTable.push(<tbody ref='tbody' key={k++} className="center" >{tableData3}</tbody>)
 
         return (
             <section>
@@ -208,16 +215,16 @@ export default class NN_InfoApplicationList extends React.Component {
                         <h1> Intend Info </h1>
                     </div>
                     <div ref="intentInfo">
-                        <table className="table detail" ref= 'master1' >
+                        <table className="table detail" ref= 'master2' >
                             {intentInfoTable}
                         </table>
                     </div>
                     <div>
-                        <h1> NER Info </h1>
+                        <h1> Story Info </h1>
                     </div>
                     <div ref="nerInfo">
-                        <table className="table detail" ref= 'master1' >
-                            {nerInfoTable}
+                        <table className="table detail" ref= 'master3' >
+                            {storyInfoTable}
                         </table>
                     </div>
                 </div>
